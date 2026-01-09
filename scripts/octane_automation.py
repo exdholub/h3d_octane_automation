662b916f8dfb4ae62bf71b6481d37fa881916724
Dmytro Holub <heap3d@gmail.com>
2026-01-09 12:06:19 +0200

    initial commit

---
+++ scripts/octane_automation.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# ================================
+# (C)2026 Dmytro Holub
+# heap3d@gmail.com
+# --------------------------------
+# modo python
+# EMAG
+# octane automation
+# ================================
+
+
+def main():
+    ...
+
+
+if __name__ == '__main__':
+    main()
